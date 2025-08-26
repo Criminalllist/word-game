68b1458177acd9355f2a325b47f733e1b1dc61eb
Vyacheslav Razlada <vrazlada@mail.ru>
2025-08-26 22:49:12 +0300

    feat: add styles & button component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <header v-if="false" class="header">
+      <h1 class="header__title">ЗАПОМНИ СЛОВО</h1>
+      <div class="header__counter life-counter">
+        <div class="life-counter__item">100</div>
+      </div>
+    </header>
+    <main class="main">
+      <AppButton />
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import AppButton from "@/common/components/AppButton.vue";
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+}
+</style>
---
+++ src/common/components/AppButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="button" type="button">
+    <slot>Начать игру</slot>
+  </button>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@use '@/assets/scss/variables.scss' as vars;
+
+.button {
+  display: grid;
+  place-items: center;
+
+  min-width: 315px;
+  padding: 16px 10px;
+  border-radius: 100px;
+  border: none;
+
+  background-color: vars.$color-dark-blue;
+  font-size: vars.$font-size-large;
+  color: vars.$color-text-white;
+  font-weight: vars.$font-weight-regular;
+}
+</style>
