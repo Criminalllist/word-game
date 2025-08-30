0986836deab6eabfd5631bc942b3309b9075b11c
Vyacheslav Razlada <vrazlada@mail.ru>
2025-08-30 18:41:59 +0300

    feat: header & card component with mock data

---
+++ src/common/components/AppIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+    :fill="fill"
+  >
+    <use :href="`#sprite-icon-${name}`" />
+  </svg>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: [String, Number],
+    default: 24,
+  },
+  height: {
+    type: [String, Number],
+    default: 24,
+  },
+  viewBox: {
+    type: String,
+    default: "0 0 24 24",
+  },
+  fill: {
+    type: String,
+    default: "currentColor",
+  },
+});
+</script>
---
+++ src/components/life-points/LifePoints.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="life-points">
+    <span class="life-points__score">{{ points ?? "-" }}</span>
+    <span class="life-points__icon">
+      <AppIcon name="life" width="24" height="24" viewBox="0 0 24 24" fill="#008BFE" />
+    </span>
+  </div>
+</template>
+
+<script setup>
+import AppIcon from "@/common/components/AppIcon.vue";
+defineProps({
+  points: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.life-points {
+  display: flex;
+  align-items: center;
+  gap: $space-xs;
+  width: max-content;
+  padding: $space-s $space-l;
+  border-radius: 36px;
+  background-color: $color-light-blue;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.15);
+
+  &__score {
+    font-weight: 700;
+    color: $color-dark-blue;
+  }
+
+  &__icon {
+    color: $color-dark-blue;
+  }
+}
+</style>
